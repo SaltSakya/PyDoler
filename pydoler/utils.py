60ea11ad9b014dc77d5a32b7310cde7dfeb9626f
SaltSakya <SaltSakya@Yahoo.com>
2024-06-13 03:55:29 +0800

    追加解压功能，追加打包 gif 的功能

--- pydoler/utils.py
+++ pydoler/utils.py
@@ -1,5 +1,7 @@
 import os
+from typing import List, Tuple
 
+import imageio
 import zipfile
 
 def sanitize_dirname(filename:str):
@@ -25,4 +27,35 @@ def zip_dir(dir_path:str, zip_file_path:str=None,
                 filename = os.path.join(root, file)
                 arcname = os.path.join(os.path.basename(root), file)
                 zf.write(filename, arcname)
+
+def unzip_file(zip_file_path:str, extract_dir:str):
+    '''### 解压文件
+    将指定zip文件解压到指定目录。
+    :param zip_file_path: zip文件路径
+    :param extract_dir: 解压目录
+    '''
+    with zipfile.ZipFile(zip_file_path, "r") as zf:
+        zf.extractall(extract_dir)
             
+def pack_gif(dir:str, gif_name:str, images:List[str], delays:List[int]|int = 30):
+    '''### 打包gif
+    将指定目录下的图片打包成gif。
+    :param dir: 目录路径
+    :param gif_name: 打包后的gif文件名
+    :param images: 图片文件名列表
+    :param delays: 每张图片的显示时间，单位毫秒
+    '''
+
+    if not gif_name.endswith(".gif"):
+        gif_name += '.gif'
+
+    if delays is int:
+        fps = 1 / delays
+    else:
+        fps = len(delays)/sum(delays)
+
+    fps *= 1000
+
+    frames = [imageio.imread(os.path.join(dir, file)) for file in images]
+    
+    imageio.mimsave(os.path.join(dir, gif_name), frames, fps=fps)
